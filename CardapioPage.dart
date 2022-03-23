73dbdd74c6960c10767d32d90958fdcde28eabd3
João Paulo <62178699+joaop2p@users.noreply.github.com>
2022-03-23 09:32:27 -0300

    CardapioPage.dart
    atualização para a segunda versão do cardápio, foi adicionado um menu de navegação

--- CardapioPage.dart
+++ CardapioPage.dart
@@ -1,76 +1,190 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 
 class CardapioPage extends StatelessWidget {
   const CardapioPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    botaoAction() {
-      Navigator.pushNamed(context, '/hotel');
-    }
-
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color.fromRGBO(230, 211, 171, 2),
-            centerTitle: true,
-            title: const Image(
-              image: AssetImage('assets/logo.png'),
-              width: 150,
-              height: 50,
-            ),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(230, 211, 171, 2),
+          centerTitle: true,
+          title: const Image(
+            image: AssetImage('assets/logo.png'),
+            width: 150,
+            height: 50,
           ),
-          body: Column(
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
             children: <Widget>[
-              Padding(padding: EdgeInsets.all(10)),
-              Center(
-                heightFactor: 2,
-                child: Text('Cardapio',
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    )),
+              DrawerHeader(
+                child: Image(
+                  image: AssetImage('assets/drawer.png'),
+                ),
+                padding: EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
               ),
-              Padding(padding: EdgeInsets.all(8)),
-              Container(
-                height: 55,
-                color: Colors.amber[600],
-                child: const Center(child: Text('Entry A')),
+              ListTile(
+                title: Text('Menu Inicial'),
+                leading: Icon(
+                  Icons.home,
+                ),
+                onTap: () {},
               ),
-              Container(
-                height: 55,
-                color: Colors.amber[500],
-                child: const Center(child: Text('Entry B')),
+              ListTile(
+                leading: Icon(
+                  Icons.assignment,
+                ),
+                title: Text('Cardápio'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/CardapioPage');
+                },
               ),
-              Container(
-                height: 55,
-                color: Colors.amber[100],
-                child: const Center(child: Text('Entry C')),
+              ListTile(
+                title: Text('Pedido'),
+                leading: Icon(
+                  Icons.shopping_basket,
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
               ),
-              Container(
-                height: 55,
-                color: Colors.amber[100],
-                child: const Center(child: Text('Entry C')),
-              ),
-              Container(
-                height: 55,
-                color: Colors.amber[100],
-                child: const Center(child: Text('Entry C')),
-              ),
-              Container(
-                height: 55,
-                color: Colors.amber[100],
-                child: Center(child: Text('Entry C')),
-              ),
-              Container(
-                height: 55,
-                color: Colors.amber[100],
-                child: Center(child: Text('Entry C')),
+              ListTile(
+                title: Text('Sair'),
+                leading: Icon(
+                  Icons.exit_to_app,
+                ),
+                onTap: () => exit(0),
               ),
             ],
-          )),
+          ),
+        ),
+        body: Column(children: <Widget>[
+          Center(
+            heightFactor: 2,
+            child: Text('Cardapio',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                )),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page4');
+              },
+              style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(375, 50))),
+              child: Center(
+                child: Text('Bebidas'),
+              ),
+            ),
+          ]),
+          Spacer(),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page5');
+              },
+              style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(375, 50))),
+              child: Center(
+                child: Text('Pratos executivos'),
+              ),
+            ),
+          ]),
+          Spacer(),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page6');
+              },
+              style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(375, 50))),
+              child: Center(
+                child: Text('Pratos p/ 2 a 4 pessoas'),
+              ),
+            ),
+          ]),
+          Spacer(),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page7');
+              },
+              style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(375, 50))),
+              child: Center(
+                child: Text('Porções'),
+              ),
+            ),
+          ]),
+          Spacer(),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page8');
+              },
+              style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(375, 50))),
+              child: Center(
+                child: Text('Ensopados'),
+              ),
+            ),
+          ]),
+          Spacer(),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page9');
+              },
+              style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(375, 50))),
+              child: Center(
+                child: Text('Bolinhos'),
+              ),
+            ),
+          ]),
+          Spacer(),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page10');
+              },
+              style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(375, 50))),
+              child: Center(
+                child: Text('Espetos'),
+              ),
+            ),
+          ]),
+          Spacer(),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page11');
+              },
+              style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(375, 50))),
+              child: Center(
+                child: Text('Petiscos'),
+              ),
+            ),
+          ]),
+          Spacer()
+        ]),
+      ),
     );
   }
 }
